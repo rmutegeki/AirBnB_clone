1eeb428a26afe3bdcbd512746a003b17779ececf
Mutegeki Ronald <murogive@gmail.com>
2022-11-26 07:24:47 +0000

    Updated Base model

--- models/base_model.py
+++ models/base_model.py
@@ -16,15 +16,19 @@ class BaseModel:
     attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
+        """initializes all the attributes of this class
+        """
         if kwargs:
+            d = "%Y-%m-%dT%H:%M:%S.%f"
             for k, v in kwargs.items():
-                if (k == 'created_at') or (k == 'updated_at'):
-                    setattr(self, k, datetime.fromisoformat(v))
-                elif k != '__class__':
+                if k == 'created_at' or k == 'updated_at':
+                    v = datetime.strptime(kwargs[k], d)
+                if k != '__class__':
                     setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now
+            self.updated_at = self.created_at
             storage.new(self)
 
     def save(self):
@@ -34,16 +38,26 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        """returns dictionary representation of an instance"""
+        """creates a new dictionary representation of an instance
+        with datetimes converted to strings
+        """
+        my_dict = {}
 
-        my_dict = dict(self.__dict__)
-        my_dict["__class__"] = self.__class__.__name__
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        for k, v in self.__dict__.items():
+            if k == "created_at" or k == "updated_at":
+                my_dict[k] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                if not values:
+                    pass
+                else:
+                    my_dict[k] = values
+        my_dict['__class__'] = self.__class__.__name__
+
         return my_dict
 
     def __str__(self):
         """string representation of an instance"""
 
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        class_name = "[" + self.__class__.__name__ + "]"
+        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(dct)
